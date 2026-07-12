9bb744e9f02e26a533270ef450847580f3b83672
Producer Minh Hieu <dj.producerminhhieu.068@gmail.com>
2026-07-13 05:51:26 +0700

    app: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/renderer",
+  base: "./",
+  build: {
+    outDir: "../../dist",
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  }
+});
+
